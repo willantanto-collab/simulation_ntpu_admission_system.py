6f3b4a46996e01f63d9bb2b62526fce4947a544c
dev-null-0xptr <willantanto@gmail.com>
2026-02-17 22:31:03 +0700

    Update ntpu_models.py
    添加华侨生录取分析逻辑：支持手动填入科目数，增加 A-Level 成绩自动校验与加权计算。

--- ntpu_models.py
+++ ntpu_models.py
@@ -14,6 +14,23 @@ class Department:
         return "录取概率高"
     else:
         return "存在风选，建议作为冲刺志向”
-
+class AdmissionAnalyser:  #通用录取分析，确保逻辑链可扩展
+  def __init__(self,grades):
+    self.grades = grades
+    self.num_subjects = totalsubjects
+  def calculate_weighted_score(self): #通用台北大学可判断的统一成绩，含金量之类的
+    #实现具体的加权计算
+    pass
+class Cambridge_Analyser(AdmissionAnalyser):  #具体cambridge 的
+  def calculate_weighted_score(self):
+    if len(self.grades) != self.num_subjects: #确认考生成绩个数和科目数量是否对的上
+      return "错误：填入成绩数与声明科目数不符”
+    actual_total = sum(self.grades)
+    average_score = actual_total /self.num_subjects
+    diffciulty_weight = 1.05 #每年可更改，不同的含金量系数
+    unified_score = average_score * difficulty_weight #通用台北大学判断的统一成绩
+    return unified_score
+    
+    
 
     
