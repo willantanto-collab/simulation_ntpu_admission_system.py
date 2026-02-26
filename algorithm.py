9ebf15d1451f547e73c48945b15ee6260098084c
dev-null-0xptr <willantanto@gmail.com>
2026-02-26 16:10:21 +0700

    Class SecurityVertifier

--- algorithm.py
+++ algorithm.py
@@ -41,5 +41,20 @@ class SecureDocument:
         for stud in self.student:
             combined += stud.hash
         return hash.lib.sha256(combined.encode()).hexdigest()) #converts content to bytes and creates a unique fingerprint for integrity vertification
-        #line 42 turn the string from human language into machine code
+        #return hash.lib.sha256(combined.encode()).hexdigest()) turn the string from human language into machine code
 #Knowledge about this class Securedocument known by self-study.
+class SecurityVertifier:
+    def vertify(self,document):
+        return self.vertify_recursive(document) #starts recursive check from root node
+    def vertify_recursive(self,node):
+        recalculated = node.content
+        for stud in node.student: #veritify all child node
+            if not self.vertify_recursive(stud): #if any student fail vertify,stop immediately
+                return False
+            recalculated += stud.hash #after child vertification,include its hash in parent's recompute
+        recalculated  = hashlib.sha256(recalculated.encode()).hexdigest() #recalculate hash for this node
+        if recalculated_hash != node.hash:
+            print(f"Change of data detected in {node.name}")
+            return False #any child tamper triggers in root invalidation
+        return True 
+    
